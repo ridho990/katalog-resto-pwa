b7b3f83a43ec6f811a66a56b2a32b8a189c895cf
Ridho Ahmad Fauzi <ridho.ahmad21@students.unila.ac.id>
2023-10-11 19:34:43 +0700

    Menambahkan dialogue eror

--- src/scripts/view/pages/detail-resto.js
+++ src/scripts/view/pages/detail-resto.js
@@ -3,7 +3,6 @@ import '../component/loader-ring';
 import '../component/detail-resto';
 import UrlParser from '../../routes/url-parser';
 import RestoApiSource from '../../data/resto-api';
-import searchBarFavorite from '../../utils/searchbar-favorite';
 import LikeButtonInitiator from '../../utils/like-button-initiator';
 
 const DetailResto = {
@@ -16,25 +15,39 @@ const DetailResto = {
     },
 
     async afterRender() {
-        const url = UrlParser.parseActiveUrlWithoutCombiner();
-        const resto = await RestoApiSource.detailResto(url.id);
-        const detailResto = document.createElement('detail-resto');
-        const mainContent = document.querySelector('#mainContent');
+        try {
+            const url = UrlParser.parseActiveUrlWithoutCombiner();
+            const resto = await RestoApiSource.detailResto(url.id);
+            const detailResto = document.createElement('detail-resto');
+            const mainContent = document.querySelector('#mainContent');
 
-        detailResto.setDetailResto = resto;
-        mainContent.appendChild(detailResto);
+            detailResto.setDetailResto = resto;
+            mainContent.appendChild(detailResto);
 
-        LikeButtonInitiator.init({
-            likeButtonContainer: document.querySelector('#likeButtonContainer'),
-            resto: {
-                id: resto.id,
-                name: resto.name,
-                rating: resto.rating,
-                city: resto.city,
-                description: resto.description,
-                pictureId: resto.pictureId
-            }
-        });
+            LikeButtonInitiator.init({
+                likeButtonContainer: document.querySelector(
+                    '#likeButtonContainer'
+                ),
+                resto: {
+                    id: resto.id,
+                    name: resto.name,
+                    rating: resto.rating,
+                    city: resto.city,
+                    description: resto.description,
+                    pictureId: resto.pictureId
+                }
+            });
+
+            DialogueError.init({
+                btnClose: document.getElementById('btn-close-x'),
+                btnYes: document.getElementById('yes-button'),
+                overlay: document.querySelector('.overlay-dialogue'),
+                wrapperDialogue: document.querySelector('.card-dialogue-eror')
+            });
+        } catch (error) {
+            alert(error);
+            console.error('Error:', error);
+        }
     }
 };
 
